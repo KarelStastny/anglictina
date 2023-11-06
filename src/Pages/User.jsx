b30c46d2bd2b1cc06cadc06530917eaf22b4d33e
Karel Šťastný <karell29@icloud.com>
2023-11-06 09:52:55 +0100

    Přidány detaily slovíček po najetí

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { UseEnglish } from "../context/EnglishContext";
 
 const User = () => {
   const { loggedUser } = UseEnglish();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
   console.log(loggedUser);
   const { firstName, secondName, allStudyProgress, studyVocabulary
@@ -13,6 +15,14 @@ const User = () => {
     timesAllPracticed: 0,
     timesCorrect: 0,
     timesIncorrect: 0,
+  };
+  const showModal = (wordDetails) => {
+    setModalContent(wordDetails);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
 
@@ -49,7 +59,7 @@ const User = () => {
         <table className="min-w-full bg-white">
           <thead>
             <tr className="w-full h-16 border-gray-300 border-b py-8">
-              <th className="text-left pl-8">Česky</th>
+              <th className="text-left pl-8 ">Česky</th>
               <th className="text-left pl-8">Anglicky</th>
               <th className="text-left pl-8">Počet všech opakování</th>
               <th className="text-left pl-8">Správně zodpovězeno</th>
@@ -57,22 +67,64 @@ const User = () => {
               <th className="text-left pl-8">Umím slovíčko?</th>
             </tr>
           </thead>
+           {/* Modální okno */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-white p-6 rounded shadow-lg w-1/2">
+              <h2 className="text-lg font-bold mb-4">{modalContent.englishWord} - {modalContent.czechWord}</h2>
+              <p><strong>Přízvuk:</strong> {modalContent.accent}</p>
+              <p><strong>Kategorie:</strong> {modalContent.animal}</p>
+              <p><strong>Náročnost:</strong> {modalContent.hard}</p>
+              <p><strong>Typ:</strong> {modalContent.type}</p>
+              <div className="mt-4">
+                <p><strong>Přípustné a podobné verze slova:</strong></p>
+                <p><strong>České:</strong></p>
+                {modalContent.czechOption.map((option, index) => (
+                  <p key={index}>{option}</p>
+                ))}
+                <p><strong>Anglické:</strong></p>
+                {modalContent.englishOption.map((option, index) => (
+                  <p key={index}>{option}</p>
+                ))}
+              </div>
+              <button 
+                onClick={closeModal}
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition ease-in-out duration-300"
+              >
+                Zavřít
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    
           <tbody>
-            {loggedUser.studyVocabulary.map((one, index) => {
-              const knowThisVocabulary = one.studyProgress[0].timesCorrect > one.studyProgress[0].timesIncorrect;
-  
-              return (
-                <tr className={`border-b border-gray-200 ${index % 2 === 0 ? "bg-gray-50" : "bg-white"}`}>
-                  <td className="text-left px-8 py-4">{one.czechWord}</td>
-                  <td className="text-left px-8 py-4">{one.englishWord}</td>
+          {loggedUser.studyVocabulary.map((one, index) => {
+            const knowThisVocabulary = one.studyProgress[0].timesCorrect > one.studyProgress[0].timesIncorrect;
+            return (
+              <tr
+                className={`border-b border-gray-200 ${index % 2 === 0 ? "bg-gray-50" : "bg-white"}`}
+                key={index}
+              >
+                {/* ... ostatní buňky tabulky ... */}
+                <td onClick={() => showModal(one)} className="group relative text-left px-8 py-4 text-blue-600 cursor-pointer ">
+                  {one.czechWord}
+
+               
+                </td>
+               
+                <td className="text-left px-8 py-4">{one.englishWord}</td>
                   <td className="text-left px-8 py-4">{one.studyProgress[0].timesPractice}</td>
                   <td className="text-left px-8 py-4">{one.studyProgress[0].timesCorrect}</td>
                   <td className="text-left px-8 py-4">{one.studyProgress[0].timesIncorrect}</td>
                   <td className={`px-8 py-4 ${knowThisVocabulary ? "text-green-500" : "text-red-500"}`}>{knowThisVocabulary ? "Ano" : "Ne"}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+                {/* ... ostatní buňky tabulky ... */}
+              </tr>
+            );
+          })}
+        </tbody>
+          
         </table>
       </div>
     </div>
--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -5,11 +5,11 @@ const LogOut = () => {
     const { setLoggedUser } = UseEnglish();
 
     return (
-        <div className="bg-red-100 p-4 rounded-lg shadow-md max-w-md mx-auto mt-8">
+        <div className=" bg-white p-4 rounded-lg shadow-md max-w-md mx-auto mt-8">
             <h2 className="text-lg font-semibold mb-4 text-center text-red-800">Zde se můžete odhlásit</h2>
             <div className="flex justify-center">
                 <button 
-                    onClick={() => setLoggedUser(null)} // Předpokládám, že null je správnější hodnota pro odhlášení
+                    onClick={() => setLoggedUser(null)} 
                     className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
                     type="button"
                     style={{ transition: "all .15s ease" }}
