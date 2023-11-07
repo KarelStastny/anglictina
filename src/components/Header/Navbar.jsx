6e1d202d0805ac5a47e1e12cb3113df1798c9d6f
Karel Šťastný <karell29@icloud.com>
2023-11-07 07:33:05 +0100

    Upraveno navigační menu pro přihlášení aby se zobrazovalo jen když uživatel není lognutý

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,77 +1,142 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { UseEnglish } from '../../context/EnglishContext';
+import { UseEnglish } from "../../context/EnglishContext";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineClose } from "react-icons/ai";
-import LogOut from '../LogOut';
+import LogOut from "../LogOut";
 
 const Navbar = () => {
-    const { loggedUser, setLoggedUser } = UseEnglish();
-    const [hamburgerMenu, setHamburgerMenu] = useState(false);
+  const { loggedUser, setLoggedUser } = UseEnglish();
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
 
-    return (
-      <nav className="bg-indigo-900 p-3 flex items-center justify-between">
-          <h1 className="text-white text-xl"><Link to={"/"}>Angličtina</Link></h1>
-          {/* Tlačítko pro mobilní menu */}
-          <div onClick={() => setHamburgerMenu(!hamburgerMenu)} className='lg:hidden z-20 cursor-pointer'>
-              {hamburgerMenu ? (
-                  <AiOutlineClose className="text-white text-3xl" />
-              ) : (
-                  <RxHamburgerMenu className="text-white text-3xl" />
-              )}
-          </div>
-          {/* Mobilní menu */}
-          <div className={`lg:hidden fixed inset-0 bg-indigo-900 p-3 space-y-4 flex flex-col items-center z-10 transform ${hamburgerMenu ? 'translate-x-0' : '-translate-x-full'} transition duration-300 ease-in-out`}>
-              <ul className="text-white">
-                  <li>
-                      <Link to="/" className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg">Home</Link>
-                  </li>
-                  <li>
-                      <Link to="/login" className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg">Přihlášení</Link>
-                  </li>
-                  {loggedUser && (
-                      <>
-                          <li>
-                              <Link to="/exercise" className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg">Procvičování</Link>
-                          </li>
-                          <li>
-                              <Link to="/user" className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg">Uživatel</Link>
-                          </li>
-                          <li>
-                              <Link to={"/"} onClick={() => setLoggedUser("")} className="text-white bg-red-600 hover:bg-red-700 px-3 py-2 rounded-md">Odhlásit se</Link>
-                          </li>
-                          
-                      </>
-                  )}
-              </ul>
-          </div>
-          {/* Menu pro velké obrazovky */}
-          <div className="hidden lg:block">
-              <ul className="flex space-x-4">
-                  <li>
-                      <Link to="/" className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md">Home</Link>
-                  </li>
-                  <li>
-                      <Link to="/login" className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md">Přihlášení</Link>
-                  </li>
-                  {loggedUser && (
-                      <>
-                          <li>
-                              <Link to="/exercise" className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md">Procvičování</Link>
-                          </li>
-                          <li>
-                              <Link to="/user" className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md">Uživatel</Link>
-                          </li>
-                          <li>
-                              <Link to={"/"} onClick={() => setLoggedUser("")} className="text-white bg-red-600 hover:bg-red-700 px-3 py-2 rounded-md">Odhlásit se</Link>
-                          </li>
-                      </>
-                  )}
-              </ul>
-          </div>
-      </nav>
-    );
-    
+  return (
+    <nav className="bg-indigo-900 p-3 flex items-center justify-between">
+      <h1 className="text-white text-xl">
+        <Link to={"/"}>Angličtina</Link>
+      </h1>
+      {/* Tlačítko pro mobilní menu */}
+      <div
+        onClick={() => setHamburgerMenu(!hamburgerMenu)}
+        className="lg:hidden z-20 cursor-pointer"
+      >
+        {hamburgerMenu ? (
+          <AiOutlineClose className="text-white text-3xl" />
+        ) : (
+          <RxHamburgerMenu className="text-white text-3xl" />
+        )}
+      </div>
+      {/* Mobilní menu */}
+      <div
+        className={`lg:hidden fixed inset-0 bg-indigo-900 p-3 space-y-4 flex flex-col items-center z-10 transform ${
+          hamburgerMenu ? "translate-x-0" : "-translate-x-full"
+        } transition duration-300 ease-in-out`}
+      >
+        <ul className="text-white">
+          <li>
+            <Link
+              to="/"
+              className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg"
+            >
+              Home
+            </Link>
+          </li>
+          {!loggedUser && (
+            <li>
+              <Link
+                to="/login"
+                className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg"
+              >
+                Přihlášení
+              </Link>
+            </li>
+          )}
+
+          {loggedUser && (
+            <>
+              <li>
+                <Link
+                  to="/exercise"
+                  className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg"
+                >
+                  Procvičování
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/user"
+                  className="hover:bg-indigo-700 px-3 py-2 rounded-md text-lg"
+                >
+                  Uživatel
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/"}
+                  onClick={() => setLoggedUser("")}
+                  className="text-white bg-red-600 hover:bg-red-700 px-3 py-2 rounded-md"
+                >
+                  Odhlásit se
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      {/* Menu pro velké obrazovky */}
+      <div className="hidden lg:block">
+        <ul className="flex space-x-4">
+          <li>
+            <Link
+              to="/"
+              className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md"
+            >
+              Home
+            </Link>
+          </li>
+          {!loggedUser && (
+            <li>
+              <Link
+                to="/login"
+                className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md"
+              >
+                Přihlášení
+              </Link>
+            </li>
+          )}
+
+          {loggedUser && (
+            <>
+              <li>
+                <Link
+                  to="/exercise"
+                  className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md"
+                >
+                  Procvičování
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/user"
+                  className="text-white hover:bg-indigo-700 px-3 py-2 rounded-md"
+                >
+                  Uživatel
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/"}
+                  onClick={() => setLoggedUser("")}
+                  className="text-white bg-red-600 hover:bg-red-700 px-3 py-2 rounded-md"
+                >
+                  Odhlásit se
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
